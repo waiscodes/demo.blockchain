01a6b54716e7490fa9ea99b5200f8601e2dff6ae
Birm Wais <b.wais@outlook.com>
2022-09-08 21:12:54 -0600

    added get methods

--- src/blockchain/useChain.ts
+++ src/blockchain/useChain.ts
@@ -1,9 +1,26 @@
-import React, { useEffect, useState } from "react";
-import SHA256 from "crypto-js/sha256";
+import { useState } from "react";
 import Block from "./Block";
 
 const useChain = () => {
 	const [chain, setChain] = useState<Block[]>([Block.createGenesisBlock()]);
+
+	const addBlock = (tx: string) => {
+		const newBlock = Block.mineBlock(chain[chain.length - 1], tx);
+		setChain([...chain, newBlock]);
+	};
+
+	const getBlockByTx = (tx: string) => {
+		return chain.filter((block) => block.tx === tx);
+	};
+
+	const getBlockByHash = (hash: string) => {
+		return chain.find((block) => block.hash === hash);
+	};
+
+	const getBlockByHeight = (height: number): Block | undefined => {
+		// height is 0-based and acts as an index
+		return chain[height];
+	};
 
 	const isChainValid = () => {
 		return chain.every((block, index) => Block.isValidBlock(chain[index - 1], block));
@@ -12,6 +29,10 @@ const useChain = () => {
 	return {
 		chain,
 		isChainValid,
+		addBlock,
+		getBlockByTx,
+		getBlockByHash,
+		getBlockByHeight,
 	};
 };
 
